938697b078af8bfe641cbcbc7ab89758b10be7a1
Camila Cidreira <camila-cidreira@hotmail.com>
2022-11-15 14:09:08 -0300

    3 versao_calculadora

--- projeto_1/Calculadora.py
+++ projeto_1/Calculadora.py
@@ -4,18 +4,66 @@ from cores import*
 
 janela = Tk()
 janela.title("Calculadora")
-janela.geometry("270x350")
-janela.config(bg = cor_fundo)
+janela.geometry("277x318")
+janela.config(bg = branco)
+janela.resizable(width=False, height=False)
 
-frame_tela = Frame(janela, width = 270, height = 70, bg = cor_janela)
+frame_tela = Frame(janela, width = 277, height = 60, bg = branco)
 frame_tela.grid(row = 0, column = 0)
 
-frame_body = Frame(janela, width = 270, height = 50, bg = cor_fundo)
+frame_body = Frame(janela, width = 277, height = 330, bg = rosa)
 frame_body.grid(row = 1, column = 0)
+
+
 
 # Criando Botões
 
-bt_C = Button(frame_body, text="C", width = 11, height = 2, bg = cor_janela)
-bt_C.place(x=0, y=0)
+bt_1 = Button(frame_body, text="C", width = 9, height = 1, bg = cinza, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_1.place(x=3, y=2)
+bt_2 = Button(frame_body, text="%", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_2.place(x=141, y=2)
+bt_3 = Button(frame_body, text="/", width = 4, height = 1, bg = verde, fg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_3.place(x=209, y=2)
+
+#Botões 2 linha
+bt_4 = Button(frame_body, text="7", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_4.place(x=3, y=53)
+bt_5 = Button(frame_body, text="8", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_5.place(x=72, y=53)
+bt_6 = Button(frame_body, text="9", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_6.place(x=140, y=53)
+bt_7 = Button(frame_body, text="*", width =4, height = 1, bg = verde, fg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_7.place(x=208, y=53)
+
+#Botões 3 linha
+bt_4 = Button(frame_body, text="4", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_4.place(x=3, y=105)
+bt_5 = Button(frame_body, text="5", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_5.place(x=72, y=105)
+bt_6 = Button(frame_body, text="6", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_6.place(x=140, y=105)
+bt_7 = Button(frame_body, text="-", width =4, height = 1, bg = verde, fg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_7.place(x=208, y=105)
+
+#Botões 4 linha
+bt_4 = Button(frame_body, text="1", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_4.place(x=3, y=157)
+bt_5 = Button(frame_body, text="2", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_5.place(x=72, y=157)
+bt_6 = Button(frame_body, text="3", width = 4, height = 1, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_6.place(x=140, y=157)
+bt_7 = Button(frame_body, text="+", width =4, height = 1, bg = verde, fg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_7.place(x=208, y=157)
+
+#Botões 5 linha
+bt_8 = Button(frame_body, text="0", width = 9, height = 0, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_8.place(x=3, y=210)
+bt_9 = Button(frame_body, text=".", width = 4, height = 0, bg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_9.place(x=141, y=210)
+bt_10 = Button(frame_body, text="=", width =4, height = 0, bg = verde, fg = branco, font = ("Verdana 0 bold"), relief = RAISED, overrelief= RIDGE)
+bt_10.place(x=208, y=210)
+
+
+
 
 janela.mainloop()
--- projeto_1/cores.py
+++ projeto_1/cores.py
@@ -1,8 +1,9 @@
 "Este é o módulo de cores do projeto calculadora"
 
-cor_fundo = "#f7a1dc" # rosa - cor dos botões
-cor_janela = "#f5f5f5" # branco - cor da janela
-
+rosa = "#dbadb3" # cor dos botões
+branco = "#f5f5f5" # cor da janela
+cinza = "#939596" # cor do botão C
+verde = "#dbadcc" # cor dos botões da direita
 
 if __name__ == "__main__":
     print(__doc__)
